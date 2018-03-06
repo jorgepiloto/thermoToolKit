23496cbc6dd57aad8e7e404738cf82c38b061e8f
Jorge Martinez <ingenierodeaviones@gmail.com>
2018-03-06 23:42:35 +0100

    Librería thermoToolKit
    Con una única función "interpolate" y un pequeño ejemplo de uso

---
+++ example.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  6 23:29:17 2018
+
+@author: Horus
+"""
+
+import thermoToolKit  
+
+### EJEMPLO DEL USO DE LA HERRAMIENTA "INTERPOLATE"
+xa = float(input("Valor para Xa: "))
+x = float(input("Valor para X: "))
+xb = float(input("Valor para Xb: "))
+ya = float(input("Valor para Ya: "))
+yb = float(input("Valor para Yb: "))
+    
+print("The value of Y is {}".format(thermoToolKit.interpolate(xa,x,xb,ya,yb)))
---
+++ thermoToolKit.py
@@ -0,0 +1,6 @@
+### LIBRERÍA DE thermoToolKit
+
+def interpolate(xa, x, xb, ya, yb):
+    
+    return ((x-xa)/(xb-xa))*(yb-ya) + ya
+    
